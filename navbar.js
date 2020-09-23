54f5db6034da5863d10f1d3f4f85e96ca9777575
RiekertHolder <71728372+RiekertHolder@users.noreply.github.com>
2020-09-23 11:44:14 +0200

    NavBar finished
    Navigation bar is finished and made responsive

--- navbar.js
+++ navbar.js
@@ -1,6 +1,37 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('nav-links')[0]
+// navigation starts here
+      
+$("#toggle").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+});
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+$("#resize ul li a").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+});
+
+$(".close-btn").click(function() {
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+});
+
+// navigation ends here
+
+// nav animation
+
+TweenMax.from("#brand", 1, {
+    delay: 0.4,
+    y: 10,
+    opacity: 0,
+    ease: Expo.easeInOut
 })
+
+TweenMax.staggerFrom("#menu li a", 1, {
+    delay: 0.4,
+    opacity: 0,
+    ease: Expo.easeInOut
+}, 0.1);
+
+// nav animation ends
+
+new WOW().init();
